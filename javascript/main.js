6029e97ab0a6f447597f6a60d07db5d94cb0bf6e
Solsantopietro <61991750+Solsantopietro@users.noreply.github.com>
2020-04-08 15:07:46 -0300

    Add events to project

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,31 @@
-console.log ("Hola mundo desde un archivo externo de JavaScript")
+//Obtener los elementos de la clase .close
+
+let links = document.querySelectorAll (".close");
+
+//Recorrerlos
+
+links.forEach(function(link){
+    
+   //Agregar un evento click a cada uno de ellos
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let content = document.querySelector('.content');
+        
+        //Quitarle las clases de animacion q ya tiene
+        content.classList.remove("zoomIn");
+        content.classList.remove("animated");
+
+        //Agregar clases para animar su salida zoomOut
+        content.classList.add("zoomOut");
+        content.classList.add("animated");
+
+        //Agrego timer
+        setTimeout(function(){
+            location.href = "/";    
+        },600)
+        
+
+        return false;
+    });
+});
+ 
